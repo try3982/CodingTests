a98add3c856893b010fb3f39518367c52b997154
WonJin <try3982@kakao.com>
2025-03-10 17:56:16 +0900

    배열: 성지키기

---
+++ src/배열/Ex01.java
@@ -0,0 +1,33 @@
+package 배열;
+
+import java.util.Scanner;
+
+// 성 지키기 : https://www.acmicpc.net/problem/1236
+public class Ex01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        char[][] map = new char[N][M];
+        for (int i = 0; i < N; i++)
+            map[i] = sc.next().toCharArray();
+
+        boolean[] rowExist = new boolean[N];
+        boolean[] colExist = new boolean[M];
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
+                if (map[i][j] == 'X') {
+                    rowExist[i] = true;
+                    colExist[j] = true;
+                }
+
+        int rowNeedCount = N;
+        int colNeedCount = M;
+        for (int i = 0; i < N; i++)
+            if (rowExist[i]) rowNeedCount--;
+        for (int i = 0; i < M; i++)
+            if (colExist[i]) colNeedCount--;
+
+        System.out.println(Math.max(rowNeedCount, colNeedCount));
+    }
+}
