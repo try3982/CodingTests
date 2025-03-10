f3e8c65d4ebb9506e6db1c5ef06588b74b14326a
WonJin <try3982@kakao.com>
2025-03-10 17:54:49 +0900

    시간복잡도 : A+B

---
+++ src/시간복잡도/Ex02.java
@@ -0,0 +1,17 @@
+package 시간복잡도;
+
+import java.util.Scanner;
+
+//A+B : https://www.acmicpc.net/problem/1000
+public class Ex02 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a= 0;
+        int b = 0;
+
+        a = in.nextInt();
+        b = in.nextInt();
+        System.out.println(a + b);
+
+    }
+}
